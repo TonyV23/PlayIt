a010b0e250243f3aa4b062b843d03ad4517d94ec
viper23 <htonnycarlos@gmail.com>
2022-04-06 11:07:05 +0200

    added method to play next and previous song and to quit the program

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,7 +1,9 @@
 package com.main;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Main {
     private static ArrayList<Album> albumsCreated = new ArrayList<>();
@@ -30,10 +32,79 @@ public class Main {
         play(PlayList1);
     }
     private static void play(LinkedList<Song> playList){
+        boolean quit = false;
+        boolean forward = true;
+        ListIterator<Song> listIterator = playList.listIterator();
+
+        if (playList.size() == 0) System.err.print("!!! This playList has no Song !!!");
+        else{
+            System.out.print("Now Playing "+listIterator.next().toString());
+            printMenu();
+        }
+        while (!quit){
+            switch (io.setINT("\nChoose An action Please :")){
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    if(!forward) {
+                        if (listIterator.hasNext()) listIterator.next();
+                        forward = true;
+                    }
+                    if (listIterator.hasNext()) System.out.print("Now playing "+listIterator.next().toString());
+                    else{
+                        System.err.print("!! No song available, reached to the end of the list !!");
+                        forward = false;
+                    }
+                    break;
+                case  2:
+                    if (forward){
+                        if (listIterator.hasPrevious()) listIterator.previous();
+                        forward = false;
+                    }
+                    if (listIterator.hasPrevious()) System.out.print("Now playing "+listIterator.previous().toString());
+                    else{
+                        System.err.print("!! No previous song available !!");
+                        forward = false;
+                    }
+                    break;
+                case 3:
+                    if (forward){
+                        if (listIterator.hasPrevious()) {
+                            System.out.print("Now playing "+listIterator.previous().toString());
+                            forward = false;
+                        }else
+                            System.err.print("!! We are at the start of the list !!");
+                    }else{
+                        if (listIterator.hasNext()){
+                            System.out.print("Now playing "+listIterator.next().toString());
+                            forward = true;
+
+                        }else System.err.print("!! We have reached to the end of list !!");
+                    }
+                    break;
+                case 4:
+                    break;
+
+                default:
+                    System.err.print("!! Wrong input please try again !!");
+                    break;
+            }
+        }
 
     }
+
+    private static void printList(LinkedList<Song> playList){
+        Iterator<Song> iterator = playList.iterator();
+        System.err.println("-----------------------------------------");
+        while (iterator.hasNext()){
+            System.out.print(iterator.next());
+        }
+        System.err.println("-----------------------------------------");
+    }
+
     private static void printMenu(){
-        int userChoice = io.setINT("" +
+        System.out.print("" +
                 "Available Options\n"+
                 "+++++++++++++++++++++\n"+
                 "0.To Quit\n"+
@@ -42,26 +113,8 @@ public class Main {
                 "3.To Replay the current song\n"+
                 "4.List of all songs"+
                 "5.Display all available options"+
-                "6.Delete current song");
-        switch (userChoice){
-            case 0:
-                System.exit(0);
-                break;
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            default:
-                System.err.print("!! Incorrect input please try again !!");
-                    break;
-        }
+                "6.Delete current song"
+        );
+
     }
 }
